ae4922de1fa45e9cd84735fa71787256f00a606e
Cristian-707 <cfub1999@gmail.com>
2025-03-24 18:26:12 -0600

    Libros

--- lib/screens/books_screen.dart
+++ lib/screens/books_screen.dart
@@ -4,6 +4,40 @@ import '../widgets/app_card.dart';
 class BooksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Libros'));
+    return ListView(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Libros recomendados para ti',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: 150,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              AppCard(
+                appName: 'La Cancion de Aquiles',
+                rating: 4.8,
+                image:
+                    'assets/images/ae574733-eaf6-40a9-86c2-96b1dafde524_Original.png',
+              ),
+              AppCard(
+                appName: 'Sapiens',
+                rating: 4.9,
+                image: 'assets/images/sapiens_book_cover.png',
+              ),
+              AppCard(
+                appName: 'Educated Book',
+                rating: 4.7,
+                image: 'assets/images/educated_book_cover.png',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
