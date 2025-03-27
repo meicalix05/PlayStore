5d9ff4967c257a965e7a26fe1ee58bb307541bfd
Tommy Vega <tommysvs@hotmail.com>
2025-03-26 22:01:52 -0600

    Se adapta los juegos a la pantalla de detalles de cada uno

--- lib/screens/games_screen.dart
+++ lib/screens/games_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../widgets/app_card.dart';
+import '/widgets/app_card.dart';
+import 'app_details_screen.dart';
 
 class GamesScreen extends StatelessWidget {
   @override
@@ -9,44 +10,151 @@ class GamesScreen extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
-            'Juega tus juegos favoritos',
+            'Juegos recomendados para ti',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ),
         Container(
-          height: 170,
+          height: 170, // Altura del contenedor para las tarjetas
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
-              AppCard(
-                appName: 'Clash of Clans',
-                rating: 5.0,
-                image: 'assets/images/COC_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Clash of Clans',
+                            description: 'Un juego de estrategia épico.',
+                            image: 'assets/images/COC_LOGO.png',
+                            rating: 5.0,
+                            downloads: '500M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Clash of Clans',
+                  rating: 5.0,
+                  image: 'assets/images/COC_LOGO.png',
+                ),
               ),
-              AppCard(
-                appName: 'Clash Royale',
-                rating: 4.8,
-                image: 'assets/images/CR_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Clash Royale',
+                            description:
+                                'Un juego de cartas y estrategia en tiempo real.',
+                            image: 'assets/images/CR_LOGO.png',
+                            rating: 4.8,
+                            downloads: '100M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Clash Royale',
+                  rating: 4.8,
+                  image: 'assets/images/CR_LOGO.png',
+                ),
               ),
-              AppCard(
-                appName: 'Wild Rift',
-                rating: 4.9,
-                image: 'assets/images/WILDRIFT_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Wild Rift',
+                            description:
+                                'League of Legends: Wild Rift es un MOBA para móviles.',
+                            image: 'assets/images/WILDRIFT_LOGO.png',
+                            rating: 4.9,
+                            downloads: '50M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Wild Rift',
+                  rating: 4.9,
+                  image: 'assets/images/WILDRIFT_LOGO.png',
+                ),
               ),
-              AppCard(
-                appName: 'Legends of Runeterra',
-                rating: 4.9,
-                image: 'assets/images/LOR_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Legends of Runeterra',
+                            description:
+                                'Un juego de cartas estratégico basado en el universo de LoL.',
+                            image: 'assets/images/LOR_LOGO.png',
+                            rating: 4.9,
+                            downloads: '10M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Legends of Runeterra',
+                  rating: 4.9,
+                  image: 'assets/images/LOR_LOGO.png',
+                ),
               ),
-              AppCard(
-                appName: 'Mario Kart Tour',
-                rating: 4.9,
-                image: 'assets/images/MARIOKARTTOUR_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Mario Kart Tour',
+                            description:
+                                'Corre con Mario y sus amigos en pistas emocionantes.',
+                            image: 'assets/images/MARIOKARTTOUR_LOGO.png',
+                            rating: 4.7,
+                            downloads: '100M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Mario Kart Tour',
+                  rating: 4.7,
+                  image: 'assets/images/MARIOKARTTOUR_LOGO.png',
+                ),
               ),
-              AppCard(
-                appName: 'Angry Birds 2',
-                rating: 4.9,
-                image: 'assets/images/ANGRYBIRDS2_LOGO.png',
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder:
+                          (context) => AppDetailsScreen(
+                            appName: 'Angry Birds 2',
+                            description:
+                                'Dispara pájaros en estructuras para derribar a los cerdos.',
+                            image: 'assets/images/ANGRYBIRDS2_LOGO.png',
+                            rating: 4.7,
+                            downloads: '100M+',
+                          ),
+                    ),
+                  );
+                },
+                child: AppCard(
+                  appName: 'Angry Birds 2',
+                  rating: 4.7,
+                  image: 'assets/images/ANGRYBIRDS2_LOGO.png',
+                ),
               ),
             ],
           ),
